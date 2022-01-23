27fc03020553faefc26e556a906628ad7c4fdc07
Willian Alves <willian.alvesb65@gmail.com>
2022-01-23 12:20:35 -0300

    Feat: Faz componentização das listas de filme vindas da API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Tmdb from './Tmdb';
+import MovieRows from './components/MovieRows';
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadingAllreq = async () => {
+      // get all list 
+      let list = await Tmdb.getHomeList()
+      setMovieList(list);
+    }
+    loadingAllreq()
+  }, [])
+
   return (
-    <div>olá will</div>
+    <div className='page'>
+      <section className='lists'>
+        {movieList.map((item, key) => (
+          <div>
+            <MovieRows
+              key={key}
+              title={item.title}
+              items={item.items}
+            />
+          </div>
+        ))}
+      </section>
+    </div>
   );
 }
